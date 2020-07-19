f8b43a5bbfcf1f3aebbfb79c45c3df411a7f255f
Charles Ivia <iviacharles@gmail.com>
2020-07-19 15:44:22 +0300

    Updates

---
+++ precache-manifest.aa84b88c756dae4f15267f4291b68cc3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fef4c2ee91c07bde6f9181eea04eab1",
+    "url": "/breaking-bad/index.html"
+  },
+  {
+    "revision": "325f89cfb9a13d04215a",
+    "url": "/breaking-bad/static/css/main.b60eb0ef.chunk.css"
+  },
+  {
+    "revision": "063a7d9a38da3de85d6e",
+    "url": "/breaking-bad/static/js/2.813b259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad/static/js/2.813b259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325f89cfb9a13d04215a",
+    "url": "/breaking-bad/static/js/main.3a32c38d.chunk.js"
+  },
+  {
+    "revision": "ef8db1de87716a30ae76",
+    "url": "/breaking-bad/static/js/runtime-main.5bea7235.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad/static/media/spinner.235b9b3e.gif"
+  }
+]);
